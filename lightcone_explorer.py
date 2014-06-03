5a369dcdc36fffd72a7faaf4e796ee8117dc0715
Loic Le Tiran <cbienpourtoi@gmail.com>
2014-06-03 19:36:22 -0300

    began working on selection. Making a table of selection filters and magnitudes

--- lightcone_explorer.py
+++ lightcone_explorer.py
@@ -35,6 +35,21 @@ def savemyplot(name):
 
 
 """
+Table of redshift bins, properties, limit magnitudes and selection filters
+"""
+
+# Make a table of the spectral axis of the Alhambra catalog
+z_bins = [2, 3, 4, 5, 6, 7]
+mag_limit = [24., 24.3, 24.5, 24.9, 24.9, 25.3]
+selec_filter_1 = ['', 'u', 'g', '', '', '']
+selec_filter_2 = ['', 'g', 'r', '', '', '']
+selec_filter_3 = ['', 'r', 'i', '', '', '']
+
+#filters = Table([filter_names,filter_leff], names=('Filter', 'Lambda eff (A)'), meta={'name': 'table of the filters'})
+
+
+
+"""
 Open files
 """
 
@@ -45,27 +60,19 @@ conepath = "./data/lightcones/"
 conename = "wmap1_bc03_"+str(file_number).zfill(3)+"_igm1.fits"
 
 hdulist = fits.open(conepath+conename)
-cone = hdulist[1].data
+#cone = hdulist[1].data
 cols = hdulist[1].columns
 hdulist.close()
 
+sys.exit()
+
 print "There are "+str(len(cone))+" objects in the cone."
-
-
 
 """
 Just playing with the files: z distribution.
 """
-																
 
-mask = cone.field('Z_APP')>2
-
-print strftime("%Y-%m-%d %H:%M:%S", gmtime())
-# selecting all objects with z>2 takes 20 min
-cone_z2 = cone[mask]
-
-print strftime("%Y-%m-%d %H:%M:%S", gmtime())
-
+'''
 fig = plt.figure()
 plt.title("Redshift distribution for the lightcone")
 plt.xlabel("Apparent Redshift (Z_APP)")
@@ -74,6 +81,21 @@ plt.hist(cone_z2['Z_APP'], bins=200)
 plt.show()
 savemyplot("z_dist")
 plt.close()
+'''
+
+
+
+"""
+Data selection
+"""
+
+mask = cone.field('Z_APP')>2
+
+print strftime("%Y-%m-%d %H:%M:%S", gmtime())
+# selecting all objects with z>2 takes 20 min
+cone_z2 = cone[mask]
+
+print strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
 
 #TODO : Make field of view to the right size
