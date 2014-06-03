4dc1982de5366569f44e4923e418580b8419ea8b
Loic Le Tiran <cbienpourtoi@gmail.com>
2014-06-03 18:08:13 -0300

    Makes a graph in redshifts and a short attempt at selecting objects

--- lightcone_explorer.py
+++ lightcone_explorer.py
@@ -12,19 +12,19 @@ http://galformod.mpa-garching.mpg.de/qa/mrobs/pages/surveys/PFS.jsp
 
 import numpy as np
 import scipy
-import pyfits
+#import pyfits
 import sys
 import os
 import glob
 import csv
 import matplotlib.pyplot as plt
 #from matplotlib.colors import LogNorm
-#import astropy
+from astropy.io import fits
 #from string import upper,lower
 #from numpy import recfromcsv
 #from numpy.random import normal
 #import math
-#from time import gmtime, strftime
+from time import gmtime, strftime
 
 
 plot_extension = ".png"
@@ -32,8 +32,6 @@ plot_directory = "./plots/"
 def savemyplot(name):
 	fig.savefig(plot_directory+name+plot_extension)
 	return
-
-
 
 
 """
@@ -46,7 +44,7 @@ file_number = 1
 conepath = "./data/lightcones/"
 conename = "wmap1_bc03_"+str(file_number).zfill(3)+"_igm1.fits"
 
-hdulist = pyfits.open(conepath+conename)
+hdulist = fits.open(conepath+conename)
 cone = hdulist[1].data
 cols = hdulist[1].columns
 hdulist.close()
@@ -58,15 +56,24 @@ print "There are "+str(len(cone))+" objects in the cone."
 """
 Just playing with the files: z distribution.
 """
+																
+
+mask = cone.field('Z_APP')>2
+
+print strftime("%Y-%m-%d %H:%M:%S", gmtime())
+# selecting all objects with z>2 takes 20 min
+cone_z2 = cone[mask]
+
+print strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
 fig = plt.figure()
 plt.title("Redshift distribution for the lightcone")
 plt.xlabel("Apparent Redshift (Z_APP)")
 plt.ylabel("#")
-plt.hist(cone['Z_APP'], bins=200)
+plt.hist(cone_z2['Z_APP'], bins=200)
 plt.show()
 savemyplot("z_dist")
-plt.clf()
+plt.close()
 
 
 #TODO : Make field of view to the right size
