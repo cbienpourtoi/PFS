a1f5d781c3a0b3f79cb7310e4c4ed661f695326e
Loic Le Tiran <cbienpourtoi@gmail.com>
2014-06-04 18:54:21 -0300

    Selection in z and magnitudes

--- lightcone_explorer.py
+++ lightcone_explorer.py
@@ -20,6 +20,7 @@ import csv
 import matplotlib.pyplot as plt
 #from matplotlib.colors import LogNorm
 from astropy.io import fits
+from astropy.table import Table
 #from string import upper,lower
 #from numpy import recfromcsv
 #from numpy.random import normal
@@ -38,14 +39,16 @@ def savemyplot(name):
 Table of redshift bins, properties, limit magnitudes and selection filters
 """
 
-# Make a table of the spectral axis of the Alhambra catalog
+dz = 0.5 # Delta in redshift (ie selection between z-dz and z+dz)
+
+
+# Make a table of the redshifts, selection filters and limit magnitude associated (limit mag always on the redder)
 z_bins = [2, 3, 4, 5, 6, 7]
 mag_limit = [24., 24.3, 24.5, 24.9, 24.9, 25.3]
-selec_filter_1 = ['', 'u', 'g', '', '', '']
-selec_filter_2 = ['', 'g', 'r', '', '', '']
-selec_filter_3 = ['', 'r', 'i', '', '', '']
-
-#filters = Table([filter_names,filter_leff], names=('Filter', 'Lambda eff (A)'), meta={'name': 'table of the filters'})
+selec_filter_1 = ['', 'SDSS_U', 'SDSS_G', 'SDSS_R', 'SDSS_I', 'Z']
+selec_filter_2 = ['', 'SDSS_G', 'SDSS_R', 'SDSS_I', 'SDSS_Z', 'Y']
+selec_filter_3 = ['SDSS_R', 'SDSS_R', 'SDSS_I', 'SDSS_Z', 'Y',      'J']
+selection_properties = Table([z_bins, mag_limit, selec_filter_1, selec_filter_2, selec_filter_3], names=('z', 'LimitMag', 'Filter1', 'Filter2', 'Filter3'), meta={'name': 'table of the selection properties'})
 
 
 
@@ -60,11 +63,10 @@ conepath = "./data/lightcones/"
 conename = "wmap1_bc03_"+str(file_number).zfill(3)+"_igm1.fits"
 
 hdulist = fits.open(conepath+conename)
-#cone = hdulist[1].data
+cone = hdulist[1].data
 cols = hdulist[1].columns
 hdulist.close()
 
-sys.exit()
 
 print "There are "+str(len(cone))+" objects in the cone."
 
@@ -89,13 +91,25 @@ plt.close()
 Data selection
 """
 
-mask = cone.field('Z_APP')>2
+for i in np.arange(len(selection_properties)):
+	mask_z = np.abs( cone.field('Z_APP') - selection_properties['z'][i] ) < dz
+	mask_mag = cone.field(selection_properties['Filter3'][i]) < selection_properties['LimitMag'][i]
+	mask = mask_z & mask_mag
+	print strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-print strftime("%Y-%m-%d %H:%M:%S", gmtime())
 # selecting all objects with z>2 takes 20 min
 cone_z2 = cone[mask]
 
 print strftime("%Y-%m-%d %H:%M:%S", gmtime())
+
+fig = plt.figure()
+plt.title("Redshift vs Redder Magnitude")
+plt.xlabel("Apparent Redshift (Z_APP)")
+plt.xlabel("Magnitude in redder color (?)")
+plt.hist2d(cone_z2['Z_APP'], cone_z2[selection_properties['Filter3'][i]], bins=40)
+plt.show()
+savemyplot("z_vs_mag")
+plt.close()
 
 
 #TODO : Make field of view to the right size
